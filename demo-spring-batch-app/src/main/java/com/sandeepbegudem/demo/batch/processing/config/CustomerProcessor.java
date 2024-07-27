1912308f84fea1143a0f76bd7c0bc5c964280369
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-07-27 18:35:07 -0500

    feat: add spring batch app

---
+++ demo-spring-batch-app/src/main/java/com/sandeepbegudem/demo/batch/processing/config/CustomerProcessor.java
@@ -0,0 +1,1 @@
+package com.sandeepbegudem.demo.batch.processing.config;import com.sandeepbegudem.demo.batch.processing.entity.Customer;import org.springframework.batch.item.ItemProcessor;public class CustomerProcessor implements ItemProcessor<Customer, Customer> {    @Override    public Customer process(Customer customer) throws Exception {        return customer;    }}
